529d0df6c33441dc03c26a11c65eba8463a84b69
ktxdev <sean.huvaya03@yahoo.com>
2022-02-04 13:50:06 +0200

    Initial commit

---
+++ src/main/java/org/ktxdev/App.java
@@ -0,0 +1,13 @@
+package org.ktxdev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
