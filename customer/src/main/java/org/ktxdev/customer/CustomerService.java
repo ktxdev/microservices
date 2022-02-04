3955e9a3de4003653bf0019d6dd3a2c3953b28e7
ktxdev <sean.huvaya03@yahoo.com>
2022-02-04 20:58:43 +0200

    Use rest template for microservices http communication

--- customer/src/main/java/org/ktxdev/customer/CustomerService.java
+++ customer/src/main/java/org/ktxdev/customer/CustomerService.java
@@ -1,9 +1,12 @@
 package org.ktxdev.customer;
 
+import lombok.val;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
+
     public void register(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRequest.firstName())
@@ -11,6 +14,16 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .email(customerRequest.email())
                 .build();
 
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        val response = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId());
+
+        if (response.isFraudster())
+            throw new IllegalStateException("Fraudster");
+
+
     }
 }
--- fraud/src/main/java/org/ktxdev/fraud/FraudCheckHistoryRestController.java
+++ fraud/src/main/java/org/ktxdev/fraud/FraudCheckHistoryRestController.java
@@ -1,13 +1,16 @@
 package org.ktxdev.fraud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/fraud-check")
 public record FraudCheckHistoryRestController(FraudCheckHistoryService fraudCheckHistoryService) {
 
     @GetMapping("{customerId}")
-    public FraudCheckResponse isFraudstar(@PathVariable int customerId) {
+    public FraudCheckResponse isFraudster(@PathVariable int customerId) {
+        log.info("### Fraud check request for customer: {}", customerId);
         boolean fraudulent = fraudCheckHistoryService.isFraudulent(customerId);
         return new FraudCheckResponse(fraudulent);
     }
